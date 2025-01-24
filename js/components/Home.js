8e03cffbe23344801ba33eedee19f92436b298ad
Jacob Krag <jacobkrag.dev@gmail.com>
2025-01-24 15:04:59 +0100

    Beta 1.1

--- js/components/Home.js
+++ js/components/Home.js
@@ -8,14 +8,14 @@ template: `
         </video>
         <div class="hero-section">
             <h1 class="textanimation">Where Ideas Take Shape</h1>
-            <div class="theme-divider">
+            <div class="theme-divider fadeIn">
                 <span></span>
                 <span></span>
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
-            <p>Great projects are built on <strong>collaboration</strong>. My goal is to turn <strong>your
+            <p class="fadeIn">Great projects are built on <strong>collaboration</strong>. My goal is to turn <strong>your
                     vision</strong> into reality with 3D models
                 that are crafted for <strong>detail, efficiency, and style.</strong></p>
         </div>
@@ -29,14 +29,42 @@ template: `
                 designing <strong>movie scenes</strong>, these assets are ready to fit <strong>seamlessly</strong> into
                 your vision.
             </p>
+            <div>
+                <h3>Your Gateway to Krag Studio <span class="highlight_1">(Roadmap)</span></h3>
+                <p>
+                    The roadmap is your gateway to everything happening at <strong>Krag Studio</strong>. Here, you can
+                    follow the progress of <strong>3D asset packs</strong> I’m developing, from initial ideas to final
+                    releases. Each pack is carefully crafted with <strong>versatility</strong> and
+                    <strong>quality</strong>
+                    in mind, tailored for use in <strong>games</strong>, <strong>animations</strong>,
+                    <strong>movies</strong>, and other creative projects.
+                </p>
+                <p>
+                    This roadmap provides a <strong>transparent look</strong> at the creative process, showcasing what’s
+                    in
+                    <strong>development</strong>, what’s <strong>near completion</strong>, and what’s <strong>coming
+                        next</strong>. Whether it’s detailed environments, immersive props, or essential models for
+                    <strong>world-building</strong>, you’ll find a clear timeline of upcoming releases.
+                </p>
+                <p>
+                    By keeping track of the roadmap, you can <strong>plan ahead</strong> and stay updated on the latest
+                    assets, ensuring you have everything you need to bring your vision to life. Explore the journey
+                    behind
+                    each project and discover how <strong>Krag Studio</strong> turns ideas into high-quality,
+                    ready-to-use
+                    <strong>3D models</strong>.
+                </p>
+            </div>
+
         </div>
         <div class="floating-divider"></div>
         <div class="next">
-            <h3>The Forgotten Room <span>(Work in progress)</span></h3>
+            <h2>Overview and roadmap:</h2>
+            <h3><span class="product">N<sup>O</sup>1</span> The Forgotten Room <span class="highlight_2">(Work in progress)</span></h3>
             <figure>
                 <img src="/assets/images/concept.webp" alt="Description of the image">
                 <figcaption>A mockup designed to illustrate the creative approach. Final assets may differ.</figcaption>
-            </figure>            
+            </figure>
         </div>
     </section>
 </main>
