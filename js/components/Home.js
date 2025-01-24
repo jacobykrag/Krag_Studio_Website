0621bd86499a6457b1a65e812cde9373fb422920
Jacob Krag <jacobkrag.dev@gmail.com>
2025-01-24 18:42:37 +0100

    Beta 1.2

--- js/components/Home.js
+++ js/components/Home.js
@@ -15,13 +15,15 @@ template: `
                 <span></span>
                 <span></span>
             </div>
-            <p class="fadeIn">Great projects are built on <strong>collaboration</strong>. My goal is to turn <strong>your
+            <p class="fadeIn">Great projects are built on <strong>collaboration</strong>. My goal is to turn
+                <strong>your
                     vision</strong> into reality with 3D models
-                that are crafted for <strong>detail, efficiency, and style.</strong></p>
+                that are crafted for <strong>detail, efficiency, and style.</strong>
+            </p>
         </div>
     </section>
     <section class="next-section">
-        <div class="next">
+        <div class="intro">
             <h2>Optimized Models for Your Projects</h2>
             <p>
                 Browse a collection of static 3D models designed for <strong>flexibility</strong>. Whether you're
@@ -60,12 +62,119 @@ template: `
         <div class="floating-divider"></div>
         <div class="next">
             <h2>Overview and roadmap:</h2>
-            <h3><span class="product">N<sup>O</sup>1</span> The Forgotten Room <span class="highlight_2">(Work in progress)</span></h3>
-            <figure>
-                <img src="/assets/images/concept.webp" alt="Description of the image">
-                <figcaption>A mockup designed to illustrate the creative approach. Final assets may differ.</figcaption>
-            </figure>
+            <h3><span class="product">N<sup>O</sup>1</span> The Forgotten Room <span class="highlight_2">(Work in
+                    progress)</span></h3>
+            <div class="roadmap">
+                <!-- Left vertical line -->
+                <aside class="timeline"></aside>
+
+                <!-- Right-side content -->
+                <aside class="roadmap-content">
+                    <figure>
+                        <h3><span class="image-title">Concept</span> <time datetime="2024-01-11">November 1,
+                                2024</time> :</h3>
+                        <img src="/assets/images/concept.webp" alt="Description of the image" loading="lazy">
+                        <figcaption>A mockup designed to illustrate the creative approach. <strong>Final assets may
+                                differ.</strong></figcaption>
+                    </figure>
+                    <figure>
+                        <h3><span class="image-title">Scene preview 1</span> <time datetime="2024-01-12">December 1,
+                                2024</time> :</h3>
+                        <img src="/assets/images/ScenePreview1.jpg" alt="Description of the image" loading="lazy">
+                    </figure>
+                    <figure>
+                        <h3><span class="image-title">Scene preview 2</span> <time datetime="2024-01-12">December 1,
+                                2024</time> :</h3>
+                        <img src="/assets/images/ScenePreview2.jpg" alt="Description of the image" loading="lazy">
+                    </figure>
+                    <figure>
+                        <h3><span class="image-title">Shaded Only</span> <time datetime="2025-01-1">January 1,
+                                2025</time> :</h3>
+                        <img src="/assets/images/ScenePreview3.jpg" alt="Description of the image" loading="lazy">
+                    </figure>
+                    <figure>
+                        <h3><span class="image-title">Flat Shading/Wireframe only</span> <time
+                                datetime="2025-01-1">January 1,
+                                2025</time> :</h3>
+                        <img src="/assets/images/Wireframe.jpg" alt="Description of the image" loading="lazy">
+                    </figure>
+                    <figure>
+                        <h3><span class="image-title">Models</span> <time datetime="2025-01-1">January 1,
+                                2025</time> :</h3>
+                        <div class="showcase">
+                            <img src="/assets/images/Bag of potatoes.jpg" alt="" loading="lazy">
+                            <img src="/assets/images/Barrel.jpg" alt="" loading="lazy">
+                            <img src="/assets/images/Bookcase.jpg" alt="" loading="lazy">
+                            <img src="/assets/images/Books.jpg" alt="" loading="lazy">
+                            <img src="/assets/images/Chair.jpg" alt="" loading="lazy">
+                            <img src="/assets/images/Coat rack.jpg" alt="" loading="lazy">
+                            <img src="/assets/images/Dreamcatcher.jpg" alt="" loading="lazy">
+                            <img src="/assets/images/Library lamp.jpg" alt="" loading="lazy">
+                            <img src="/assets/images/Old lamp.jpg" alt="" loading="lazy">
+                            <img src="/assets/images/Old stool.jpg" alt="" loading="lazy">
+                            <img src="/assets/images/Old table.jpg" alt="" loading="lazy">
+                            <img src="/assets/images/Plate with candles.jpg" alt="" loading="lazy">
+                            <img src="/assets/images/Raditator.jpg" alt="" loading="lazy">
+                            <img src="/assets/images/Suitcases.jpg" alt="" loading="lazy">
+                        </div>
+                    </figure>
+                    <figure>
+                        <h3><span class="image-title">Planned Release Date</span> <time datetime="2025-03-2">February 3,
+                                2025</time> :</h3>
+                        <img src="/assets/images/message.jpg" alt="Description of the image" loading="lazy">
+                        <figcaption> <strong>Planned Release Date:</strong> February 3, 2025
+                            <br>
+                            Mark your calendars! The <strong>final version</strong> of this 3D asset pack is set to
+                            launch on <strong>February 3, 2025</strong>. Carefully crafted to ensure the <strong>highest
+                                quality</strong>, this release will provide you with <strong>versatile</strong> and
+                            <strong>polished assets</strong>, perfect for your creative projects.
+                            <br><br>
+                            Additionally, all <strong>future updates</strong> to this pack will be provided <strong>free
+                                of charge</strong>, ensuring you always have access to the <strong>latest
+                                improvements</strong> and additions.
+                        </figcaption>
+                    </figure>
+                </aside>
+            </div>
+            <div class="divider">
+                <div class="dashed-line effect1">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <h3><span class="product">N<sup>O</sup>2</span> Project Dragoon <span class="highlight_2">(Work in
+                    progress)</span></h3>
+            <div class="roadmap">
+                <!-- Left vertical line -->
+                <aside class="timeline"></aside>
+
+                <!-- Right-side content -->
+                <aside class="roadmap-content">
+                    <figure>
+                        <h3><span class="image-title">Concept</span> <time datetime="2025-14-1">January 14,
+                                2025</time> :</h3>
+                        <iframe width="560" height="315"
+                            src="https://www.youtube.com/embed/J8kHpOwSovA?si=d3n00RiKuZfBIqRm"
+                            title="YouTube video player" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <figcaption><strong>Project Dragoon:</strong> Created in <strong>Unreal Engine 5</strong>, this
+                            desolate desert scene guards a solemn mystery. A <strong>lone sword rests in stone</strong>,
+                            surrounded by <strong>ancient seals</strong> that must never be broken. As a
+                            <strong>tattered flag</strong> flutters in the arid winds, whispers of a <strong>forbidden
+                                past</strong> echo through the sands. <strong>Final assets may
+                                differ.</strong></figcaption>
+                    </figure>
+                </aside>
+            </div>
         </div>
+        <div class="floating-divider"></div>
     </section>
 </main>
 
