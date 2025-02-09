c03d14b15b70f5b39cd5f39dee5a599bcee93bae
Jacob Krag <jacobkrag.dev@gmail.com>
2025-02-09 11:15:36 +0100

    Roadmap update version 2.2

--- js/app.js
+++ js/app.js
@@ -3,42 +3,42 @@ import About from './components/About.js';
 import Contact from './components/Contact.js';
 import NotFound from './components/NotFound.js';
 import PrivacyPolicy from './components/privacy_policy.js';
-import CookieBanner from './components/CookieBanner.js'; // Import the CookieBanner component
+import CookieBanner from './components/CookieBanner.js';
 
-// Define the routes
+
 const routes = [
     { path: '/', component: Home },
     { path: '/about', component: About },
     { path: '/contact', component: Contact },
     { path: '/privacy-policy', component: PrivacyPolicy },
 
-    // Catch-all route for 404
+
     { path: '/:pathMatch(.*)*', component: NotFound },
 ];
 
-// Create the router instance
+
 const router = VueRouter.createRouter({
-    history: VueRouter.createWebHashHistory(), // Use hash-based routing for simplicity
+    history: VueRouter.createWebHashHistory(),
     routes,
     scrollBehavior(to, from, savedPosition) {
         if (savedPosition) {
-            // If there's a saved scroll position (e.g., back/forward button), use it
+            
             return savedPosition;
         } else {
-            // Scroll to the top of the page by default
+            
             return { top: 0, left: 0 };
         }
     },
 });
 
-// Create the Vue app
+
 const app = Vue.createApp({});
 
-// Register components
+
 app.component('cookie-banner', CookieBanner);
 
-// Use the router
+
 app.use(router);
 
-// Mount the app
+
 app.mount('#app');
--- js/script.js
+++ js/script.js
@@ -12,14 +12,14 @@ $(document).ready(function () {
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-    const header = document.querySelector('header'); // Select the header element
+    const header = document.querySelector('header');
 
     window.addEventListener('scroll', () => {
         if (window.scrollY > 50) {
-            // Add a class when scrolling down
+            
             header.classList.add('scrolled');
         } else {
-            // Remove the class when at the top
+            
             header.classList.remove('scrolled');
         }
     });
